10dc6677702f8422cef20aac4e6bb8feb56dce3c
Gl3nutzu <preadadaniel06@gmail.com>
2023-02-07 23:37:22 +0200

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [cname, setcname] = useState("");
+  const [location, setlocation] = useState("");
+  const [duration, setduration] = useState("");
+  const [starts, setstarts] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("da!");
+    let obj = {
+      "name": cname,
+      "location": location,
+      "duration": duration,
+      "starts": starts
+    }
+    axios.post("http://localhost:3000/api/course/addcourse", obj).then(res => alert('Succes'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form style={{display: "flex", flexDirection: "column", gap: "10px"}} onSubmit={handleSubmit}>
+      <label>
+        Course name:
+        <input type="text" name="name" onChange={(e) => {setcname(e.target.value)}} />
+      </label>
+      <label>
+        Location:
+        <input type="text" name="location" onChange={(e) => {setlocation(e.target.value)}} />
+      </label>
+      <label>
+        Duration:
+        <input type="text" name="duration" onChange={(e) => {setduration(e.target.value)}} />
+      </label>
+      <label>
+        Available starts:
+        <input type="text" name="starts" onChange={(e) => {setstarts(e.target.value)}} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
